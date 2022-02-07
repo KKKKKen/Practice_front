7cf81332772c54ea48cf931cb0005cc91239150b
Ken <ken@tajimakentonoMacBook-Air.local>
2022-02-07 11:28:53 +0900

    useQuery

---
+++ apollo-client.js
@@ -0,0 +1,19 @@
+import {
+  ApolloClient,
+  InMemoryCache,
+  // gql
+} from "@apollo/client";
+// 手順としてまずはライブラリをインストールして → new ApolloClientでinitialize（初期化にはuri, cacheつまりデータの一時保存が必要）して、
+// このファイルのコードはindex.js内に書いてもいいのかな？？分けた方が見やすいのか
+const client = new ApolloClient({
+    uri: "https://countries.trevorblades.com",
+    cache: new InMemoryCache()
+});
+
+// 実際のデータの取得
+// const client = new ApolloClient({
+//   uri: 'http://localhost:3000/graphql',
+//   cache: new InMemoryCache()
+// });
+
+export default client;
