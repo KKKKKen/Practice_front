715ebed68be8430c6ceb1902ea2ee5e22f16b586
Ken <ken@tajimakentonoMacBook-Air.local>
2022-02-11 12:59:23 +0900

    とりあえずcommit、あとどうやってuseMutationするかを調べる

--- components/cratePost.js
+++ components/cratePost.js
@@ -1,4 +1,6 @@
 import { gql, useMutation } from '@apollo/client'
+// inputのvalueにstateを入れてstate経由でユーザーから入力された値を取得する方法もあるがそれよりもスマートなのがuseRef！
+import React, { useRef } from 'react'
 
 // 定義
 export const CREATE_POST = gql`
@@ -13,43 +15,44 @@ export const CREATE_POST = gql`
 export function CreatePost() {
   const [createPost, {data, loading, error}] = useMutation(CREATE_POST);
 
-  if (loading) return 'Submitting...';
-  if (error) return `Submission error! ${error.message}`;
-
-
   // const handleClick = (title: String) {
 
   // }
+  // preventDefault()で送信した時にリダイレクトをしないようにする
+  const handleClick = e => {
+    e.preventDefault();
 
-  handleClick(){
-    this.create_post()
+
   }
 
+  // useRef（inputからvalueを取得するために使う）
+  const inputTitleRef = useRef(null)
+  const inputContentRef = useRef(null)
 
 
-
+// デバッグはいつでも出力させること！Golangではfmt.Printf(%v, 出力したい変数名)でやっていたように
+// フロントではconcole.log()
   return (
     <div>
     <form
       onSubmit={e => {
         e.preventDefault();
-        CreatePost({
-           variables: { 
-             title: input.value,
-             content: input.value
-            } });
+        console.log(e.target.text.value)
+        CreatePost({ variables: { type: inputTitleRef } });
         input.value = '';
       }}
     >
       <p>title</p>
       <input
+        ref={inputTitleRef}
         className='title'
       />
       <p>post</p>
       <input 
         className='content'
+        ref={inputContentRef}
       />
-      <button type="submit" onClick='{this.handleClick}'>Add Todo</button>
+      <button type="submit">Add Todo</button>
     </form>
     </div>
 
