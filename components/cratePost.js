edb2481e58659290f74d3923c69a5fa7698882c3
Ken <ken@tajimakentonoMacBook-Air.local>
2022-02-11 18:30:13 +0900

    useRefで出来なさそうなのでstateを使う

--- components/cratePost.js
+++ components/cratePost.js
@@ -1,7 +1,8 @@
 // フック＝関数コンポーネントにstateやライフサイクルなどの機能を接続する（hook into）ための機能
-import React, { useRef } from 'react'
+import React, { useRef, useState} from 'react'
 import { gql } from '@apollo/client'
 import { useMutation } from '@apollo/client'
+import { storeKeyNameFromField } from '@apollo/client/utilities'
 // import { useMutation } from '@apollo/react-hooks'
 // import { useMutation } from '@apollo/react-hooks'
 // inputのvalueにstateを入れてstate経由でユーザーから入力された値を取得する方法もあるがそれよりもスマートなのがuseRef！
@@ -31,8 +32,10 @@ const CREATE_POST = gql`
   // top-levelでのみフック（useStateとか）を呼び出す必要がある
   // const [createPost, {data, loading, error}] = useMutation(CREATE_POST);
   // let title, content;
+  const [title, setTitle]  = useState('')
+  const [content, setContent] = useState('')
   const [createPost, {data}] = useMutation(CREATE_POST)
-
+  console.log(title+'state')
   // const handleClick = (title: String) {
 
   // }
@@ -46,6 +49,9 @@ const CREATE_POST = gql`
   const inputTitleRef = useRef(null)
   const inputContentRef = useRef(null)
   console.log(inputTitleRef+'デバッグ')
+  console.log(title+'state')
+  
+
 
 // デバッグはいつでも出力させること！Golangではfmt.Printf(%v, 出力したい変数名)でやっていたように
 // フロントではconcole.log()
@@ -56,19 +62,22 @@ const CREATE_POST = gql`
         e.preventDefault();
         // console.log(e.target.text.value)
         console.log(inputTitleRef+'kぢ')
-        createPost({ variables: { title: inputTitleRef, content: inputContentRef } });
+        createPost({ variables: { title, content } });
+        // createPost({ variables: { title: title, content: content } });
         // useMutationを含む変数をここで使うのかな？？
       }}
     >
       <p>title</p>
       <input
         ref={inputTitleRef}
+        onChange={e => setTitle(e.target.value)}
         className='title'
       />
       <p>post</p>
       <input 
-        className='content'
         ref={inputContentRef}
+        onChange={e => setContent(e.target.value)}
+        className='content'
       />
       <button type="submit">Add Todo</button>
     </form>
